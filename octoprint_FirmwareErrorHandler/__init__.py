d392b0993d646cba5da5e99649c18a1642fe649d
jneilliii <jneilliii+octoprint@gmail.com>
2019-10-03 17:50:23 -0400

    0.1.1 - Added Python 3 compatibility

--- octoprint_FirmwareErrorHandler/__init__.py
+++ octoprint_FirmwareErrorHandler/__init__.py
@@ -80,6 +80,7 @@ class FirmwareErrorHandler(octoprint.plugin.AssetPlugin,
 		)
 
 __plugin_name__ = "FirmwareErrorHandler"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
 	global __plugin_implementation__
